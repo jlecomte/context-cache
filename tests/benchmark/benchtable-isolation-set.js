37d42f8296e0a63aba6e3c53215c84e684356fbc
Caridy Patino <caridy@gmail.com>
2013-01-28 13:13:19 -0500

    moving tests around for better organization. more benchmarks coming soon, so better to be prepared ;)

---
+++ tests/benchmark/benchtable-isolation-set.js
@@ -0,0 +1,42 @@
+/*jslint node:true, nomen:true, stupid: true */
+
+'use strict';
+
+var common = require('./benchmark-common.js'),
+    ContextCache = require('../../lib/context-cache.js');
+
+var v0 = ContextCache.create(),
+
+    v1 = ContextCache.create({
+        isolationMode: 'json'
+    }),
+
+    v2 = ContextCache.create({
+        isolationMode: 'clone'
+    }),
+
+    v3 = ContextCache.create({
+        isolationMode: 'proto'
+    });
+
+common.getSuiteTable('context set method')
+    // add functions
+    .addFunction('set without isolation', function (s) {
+        v0.set(common.getContext(), s);
+    })
+    .addFunction('set with json isolation', function (s) {
+        v1.set(common.getContext(), s);
+    })
+    .addFunction('set with clone isolation', function (s) {
+        v2.set(common.getContext(), s);
+    })
+    .addFunction('set with proto isolation', function (s) {
+        v3.set(common.getContext(), s);
+    })
+    // add inputs
+    .addInput('Simple object', [common.getSimpleObject()])
+    .addInput('3 levels deep object', [common.get3LevelsObject()])
+    .addInput('10 levels deep object', [common.get10LevelsObject()])
+    .addInput('Extremely big object', [common.getBigObject()])
+    // spin it!
+    .run();
